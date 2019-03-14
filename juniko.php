7f5c18418cd25989f554e87831906573caad8eb7
Junikod <48554671+Junikod@users.noreply.github.com>
2019-03-14 19:30:06 +0700

    tugas 1
    Hello World

---
+++ juniko.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>Tugas Php</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+   <?php echo '<p>Nama: Juniko Dwiki Saputro</p>'; ?> 
+    <?php echo '<p>NIM: 16.01.53.0017</p>'; ?> 
+ </body>
+</html>
